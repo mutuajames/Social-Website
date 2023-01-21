8e1953868d4b50a0cc7d3c379ddf4f48c8fd295c
mutuajames <njorog3.james@gmail.com>
2023-01-21 15:00:05 +0300

    Users cannot use an existing email

--- core/account/forms.py
+++ core/account/forms.py
@@ -22,6 +22,12 @@ class RegistrationForm(forms.ModelForm):
       raise forms.ValidationError('Passwords don\'t match.')
     return cd['password2']
 
+  def clean_email(self):
+    data = self.cleaned_data['email']
+    if User.objects.filter(email=data).exists():
+      raise forms.ValidationError('Email already exists')
+    return data
+
 
 class UserEditForm(forms.ModelForm):
   class Meta:
